cc1204f0164b573ea569d4b584cf693a17d97686
ich777 <christophhummer@gmail.com>
2023-03-28 15:34:41 +0200

    Dateien hochladen nach „source/usr/local/emhttp/plugins/iscsi-initiator/include“

--- source/usr/local/emhttp/plugins/iscsi-initiator/include/exec.sh
+++ source/usr/local/emhttp/plugins/iscsi-initiator/include/exec.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 
-function get_initiator_name(){
-echo -n "$(cat /boot/config/plugins/iscsi-initiator/initiatorname.cfg | cut -d '=' -f2)"
-}
-
 function change_initiator_name(){
 echo -n "$(sed -i "/InitiatorName=/c\InitiatorName=${1}" "/boot/config/plugins/iscsi-initiator/initiatorname.cfg")"
 echo -n "$(iscsiadm -m node --logout)"
 echo -n "$(kill $(pidof iscsid))"
-echo -n "$(iscsid --config=/boot/config/plugins/iscsi-initiator/iscsid.conf --initiatorname=/boot/config/plugins/iscsi-initiator/initiatorname.cfg))"
-}
-
-function get_active_sessions(){
-echo -n "$(iscsiadm --mode session)"
+echo -n "$(iscsid --config=/boot/config/plugins/iscsi-initiator/iscsid.conf --initiatorname=/boot/config/plugins/iscsi-initiator/initiatorname.cfg)"
 }
 
 function create_target(){
-echo -n "$(iscsiadm -m discovery -t sendtargets -p ${2}:${3})"
+echo "Trying to connect to ${2}:${3}"
+timeout 5 iscsiadm -m discovery -t sendtargets -p ${2}:${3} > /dev/null 2>&1
+EXIT_STATUS="$?"
+if [ "${EXIT_STATUS}" == 0 ]; then
+  echo "Connection to ${2}:${3} successful"
+  unset EXIT_STATUS
+else
+  echo "ERROR: Connecton to ${2}:${3} timed out"
+  exit 1
+fi
 sleep 0.5
-echo -n "$(iscsiadm -m node -T ${1} -p ${2}:${3} --login)"
-echo -n "$(echo "$1 $2 $3" >> /boot/config/plugins/iscsi-initiator/targets.cfg)"
+echo
+iscsiadm -m node -T ${1} -p ${2}:${3} --login
+EXIT_STATUS="$?"
+if [ "${EXIT_STATUS}" == 0 ]; then
+  echo -n "$(echo "$1 $2 $3" >> /boot/config/plugins/iscsi-initiator/targets.cfg)"
+elif [ "${EXIT_STATUS}" == 15 ]; then
+  echo "WARNING: Already connected to target ${1} on ${2}:${3}, not connecting again!"
+  exit 1
+elif [ "${EXIT_STATUS}" == 21 ]; then
+  echo "ERROR: No target with name ${1} found on: ${2}:${3}"
+  exit 1
+else
+  echo "ERROR: iSCSI connection not successful!"
+  echo "Error Code: ${EXIT_STATUS}"
+  exit 1
+fi
 }
 
 function remove_target(){
-echo -n "$(iscsiadm -m node -T ${1} -p ${2}:${3} --logout)"
-echo -n "$(sed -i "/$1 $2 $3/d" /boot/config/plugins/iscsi-initiator/targets.cfg)"
+iscsiadm -m node -T ${1} -p ${2}:${3} --logout
+sed -i "/$1 $2 $3/d" /boot/config/plugins/iscsi-initiator/targets.cfg
+}
+
+function reconnect_single(){
+iscsiadm -m node -T ${1} -p ${2}:${3} --logout
+sleep 0.5
+echo
+timeout 5 iscsiadm -m discovery -t sendtargets -p ${2}:${3} > /dev/null 2>&1
+iscsiadm -m node -T ${1} -p ${2}:${3} --login
 }
 
 $@
-
